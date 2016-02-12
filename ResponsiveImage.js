37881a18dfda3b8e970624a354e28f8478d722f7
Brent Vatne <brentvatne@gmail.com>
2016-02-12 11:04:12 -0800

    Move getClosestHighQualitySource to static method

--- ResponsiveImage.js
+++ ResponsiveImage.js
@@ -9,25 +9,7 @@ let {
 
 class ResponsiveImage extends React.Component {
 
-  setNativeProps(nativeProps) {
-    this.refs.image.setNativeProps(nativeProps);
-  }
-
-  render() {
-    let { source } = this.props;
-    let optimalSource = this._getClosestHighQualitySource();
-    if (optimalSource) {
-      source = optimalSource;
-    }
-    if (!source) {
-      throw new Error(`Couldn't find an appropriate image source`);
-    }
-
-    return <Image {...this.props} ref="image" source={source} />;
-  }
-
-  _getClosestHighQualitySource() {
-    let { sources } = this.props;
+  static getClosestHighQualitySource(sources) {
     let pixelRatios = Object.keys(sources);
     if (!pixelRatios.length) {
       return null;
@@ -44,6 +26,23 @@ class ResponsiveImage extends React.Component {
 
     let largestPixelRatio = pixelRatios[pixelRatios.length - 1];
     return sources[largestPixelRatio];
+  }
+
+  setNativeProps(nativeProps) {
+    this.refs.image.setNativeProps(nativeProps);
+  }
+
+  render() {
+    let { source } = this.props;
+    let optimalSource = ResponsiveImage.getClosestHighQualitySource(this.props.sources);
+    if (optimalSource) {
+      source = optimalSource;
+    }
+    if (!source) {
+      throw new Error(`Couldn't find an appropriate image source`);
+    }
+
+    return <Image {...this.props} ref="image" source={source} />;
   }
 }
 
